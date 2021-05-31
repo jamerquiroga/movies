6934cbe5580f4ec46cd8a62b462dc2a8f23d60e1
Jamer Quiroga <jamer.quiroga@avantica.com>
2021-05-31 18:43:23 -0500

    add annotation to provide dependency to the activity

--- presentation/src/main/java/com/jquiroga/movies/features/popular/view/MainActivity.kt
+++ presentation/src/main/java/com/jquiroga/movies/features/popular/view/MainActivity.kt
@@ -2,13 +2,20 @@ package com.jquiroga.movies.features.popular.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.jquiroga.movies.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel.getMovies()
     }
 
 }
